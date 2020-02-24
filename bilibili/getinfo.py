2a582f77245d4994c4bdb8ee4ecf64f938dae8fb
chensk <290714607@qq.com>
2020-02-24 15:56:39 +0800

    获取用户信息

--- bilibili/getinfo.py
+++ bilibili/getinfo.py
@@ -3,6 +3,10 @@ import json
 class GetInfo:
     cookies = 0
     json = 0
+    uname = 0;
+    mid = 0;
+    following = 0;
+
     def __init__(self):
 
         pass
@@ -13,8 +17,15 @@ class GetInfo:
         print ('init结束')
 
     def getuinfo(self):
-        pass;
+        url = "https://api.bilibili.com/x/web-interface/nav";
+        self.json  = json.loads(self.get(url))
+        self.uname = self.json['data']['uname']
+        self.mid = self.json['data']['wallet']['mid']
 
+        url = "https://api.bilibili.com/x/relation/stat?jsonp=jsonp&vmid="+str(self.mid)
+        self.json  = json.loads(self.get(url))
+        self.following = self.json['data']['following']
+        return  self;
     def get(self, url, data={}):
         headers = {
             "Accept": "*/*",
@@ -24,7 +35,7 @@ class GetInfo:
         }
 
         r = requests.get(url=url, headers=headers, params=data, timeout=10, verify=False)
-        print(r.content.decode());
+
         return r.content.decode()
 
 
--- getinfo.py
+++ getinfo.py
@@ -1,8 +1,8 @@
 import bilibili.getinfo as bGetinfo
 
-bilibilicookies = "_uuid=B2263352-83C8-E817-4011-C5A862BBC19B25528infoc; buvid3=EAC1EDF5-60B4-4461-8E46-D8B4B81B6E14155832infoc; CURRENT_FNVAL=16; rpdid=|(umRJl|||mJ0J'ul~)lJ)~Jk; LIVE_BUVID=AUTO4715785517982545; im_notify_type_25437908=0; CURRENT_QUALITY=80; INTVER=1; sid=d448x1gs; DedeUserID=25437908; DedeUserID__ckMd5=516bfbcc2136a97d; SESSDATA=228af7e9%2C1585093835%2C48daa321; bili_jct=4d91f1d39d2a066feaf0c274622ad44c; bp_t_offset_25437908=359378270349537868"
+bilibilicookies = "SESSDATA=228af7e9%2C1585093835%2C48daa321; bili_jct=4d91f1d39d2a066feaf0c274622ad44c;"
 
 
 bGetinfo = bGetinfo.GetInfo()
 bGetinfo.init(bilibilicookies)
-bGetinfo.getuinfo();
+print(bGetinfo.getuinfo().following);
